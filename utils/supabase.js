be81ed39b789a1fef14666e2d6718c10dab6a86c
Ishaan Saxena <ishaansaxena145@gmail.com>
2025-09-21 03:01:23 +0530

    Create supabase.js

---
+++ utils/supabase.js
@@ -0,0 +1,10 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(https://pgkmnqgihgmzlwbmknne.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBna21ucWdpaGdtemx3Ym1rbm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0MDIwODIsImV4cCI6MjA3Mzk3ODA4Mn0.Qx3KCFbJPgvAMzaAEiawEb3ck_RoMERd2z__xlILhUw);
+
+export default supabase
+        
